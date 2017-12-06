c3feac873c446cb31d7f201d6560288cfb6d4764
sahirug <sahirug@gmail.com>
2017-12-07 03:26:15 +0530

    Further developed graph pages. Implemented google material bar chart to show incidents by city

--- app/Http/Controllers/GraphController.php
+++ app/Http/Controllers/GraphController.php
@@ -89,7 +89,15 @@ class GraphController extends Controller
         }
         else
         {
-
+            if($request->isMethod('post'))
+            {
+                $request->validate(
+                    [
+                        'startDate' => 'required|before:endDate',
+                        'endDate' => 'required|after:startDate',
+                    ]
+                );
+            }
         }
 
         return view('public.view_graph', $data);
@@ -97,11 +105,100 @@ class GraphController extends Controller
 
     public function sortByDistrict($district)
     {
-        $district_incidents = $this->incident->select(DB::raw('type, status, count(*) as number_of_incidents'))
+        $data['title'] = $district;
+        $data['onGraph'] = 1;
+        switch (session('type'))
+        {
+            case 'admin':
+                $data['username'] = Auth::user()->username;
+                $data['user_type'] = Auth::user()->type;
+                $data['template'] = 'templates.admin_template';
+                break;
+            case 'user':
+                $data['username'] = Auth::user()->username;
+                $data['type'] = Auth::user()->type;
+                $data['template'] = 'templates.user_template';
+                break;
+            default:
+                $data['template'] = 'templates.public_template';
+                break;
+        }
+        $district_incidents = $this->incident->select(DB::raw('city, type, status, count(*) as number_of_incidents'))
             ->where('district',$district)
             ->where('status','approved')
+            ->groupBy('city')
+            ->get();
+//        dd($district_incidents->toArray());
+        if($district == 'Colombo')
+        {
+            $data['cities']['Colombo']['Landslide'] = $this->getIncidentByCityAndType('Colombo', 'Landslide');
+            $data['cities']['Colombo']['Flood'] = $this->getIncidentByCityAndType('Colombo', 'Flood');
+            $data['cities']['Colombo']['Thunderstorm'] = $this->getIncidentByCityAndType('Colombo', 'Thunderstorm');
+            $data['cities']['Colombo']['Fire'] = $this->getIncidentByCityAndType('Colombo', 'Fire');
+            $data['cities']['Colombo']['Other'] = $this->getIncidentByCityAndType('Colombo', 'Other');
+
+            $data['cities']['Dehiwela']['Landslide'] = $this->getIncidentByCityAndType('Dehiwela', 'Landslide');
+            $data['cities']['Dehiwela']['Flood'] = $this->getIncidentByCityAndType('Dehiwela', 'Flood');
+            $data['cities']['Dehiwela']['Thunderstorm'] = $this->getIncidentByCityAndType('Dehiwela', 'Thunderstorm');
+            $data['cities']['Dehiwela']['Fire'] = $this->getIncidentByCityAndType('Dehiwela', 'Fire');
+            $data['cities']['Dehiwela']['Other'] = $this->getIncidentByCityAndType('Dehiwela', 'Other');
+
+            $data['cities']['Moratuwa']['Landslide'] = $this->getIncidentByCityAndType('Moratuwa', 'Landslide');
+            $data['cities']['Moratuwa']['Flood'] = $this->getIncidentByCityAndType('Moratuwa', 'Flood');
+            $data['cities']['Moratuwa']['Thunderstorm'] = $this->getIncidentByCityAndType('Moratuwa', 'Thunderstorm');
+            $data['cities']['Moratuwa']['Fire'] = $this->getIncidentByCityAndType('Moratuwa', 'Fire');
+            $data['cities']['Moratuwa']['Other'] = $this->getIncidentByCityAndType('Moratuwa', 'Other');
+
+            $data['cities']['Sri Jayawardenepura Kotte']['Landslide'] = $this->getIncidentByCityAndType('Sri Jayawardenepura Kotte', 'Landslide');
+            $data['cities']['Sri Jayawardenepura Kotte']['Flood'] = $this->getIncidentByCityAndType('Sri Jayawardenepura Kotte', 'Flood');
+            $data['cities']['Sri Jayawardenepura Kotte']['Thunderstorm'] = $this->getIncidentByCityAndType('Sri Jayawardenepura Kotte', 'Thunderstorm');
+            $data['cities']['Sri Jayawardenepura Kotte']['Fire'] = $this->getIncidentByCityAndType('Sri Jayawardenepura Kotte', 'Fire');
+            $data['cities']['Sri Jayawardenepura Kotte']['Other'] = $this->getIncidentByCityAndType('Sri Jayawardenepura Kotte', 'Other');
+            $data['district'] = 'Colombo';
+        }
+        else if($district == 'Gampaha')
+        {
+            $data['cities']['Gampaha']['Landslide'] = $this->getIncidentByCityAndType('Gampaha', 'Landslide');
+            $data['cities']['Gampaha']['Flood'] = $this->getIncidentByCityAndType('Gampaha', 'Flood');
+            $data['cities']['Gampaha']['Thunderstorm'] = $this->getIncidentByCityAndType('Gampaha', 'Thunderstorm');
+            $data['cities']['Gampaha']['Fire'] = $this->getIncidentByCityAndType('Gampaha', 'Fire');
+            $data['cities']['Gampaha']['Other'] = $this->getIncidentByCityAndType('Gampaha', 'Other');
+
+            $data['cities']['Negombo']['Landslide'] = $this->getIncidentByCityAndType('Negombo', 'Landslide');
+            $data['cities']['Negombo']['Flood'] = $this->getIncidentByCityAndType('Negombo', 'Flood');
+            $data['cities']['Negombo']['Thunderstorm'] = $this->getIncidentByCityAndType('Negombo', 'Thunderstorm');
+            $data['cities']['Negombo']['Fire'] = $this->getIncidentByCityAndType('Negombo', 'Fire');
+            $data['cities']['Negombo']['Other'] = $this->getIncidentByCityAndType('Negombo', 'Other');
+            $data['district'] = 'Gampaha';
+        }
+        else if($district == 'Kalutara')
+        {
+            $data['cities']['Kalutara']['Landslide'] = $this->getIncidentByCityAndType('Kalutara', 'Landslide');
+            $data['cities']['Kalutara']['Flood'] = $this->getIncidentByCityAndType('Kalutara', 'Flood');
+            $data['cities']['Kalutara']['Thunderstorm'] = $this->getIncidentByCityAndType('Kalutara', 'Thunderstorm');
+            $data['cities']['Kalutara']['Fire'] = $this->getIncidentByCityAndType('Kalutara', 'Fire');
+            $data['cities']['Kalutara']['Other'] = $this->getIncidentByCityAndType('Kalutara', 'Other');
+            $data['district'] = 'Kalutara';
+        }
+        else
+        {
+            dd('error district not found');
+        }
+//        dd($data);
+        return view('public.district', $data);
+    }
+
+    public function getIncidentByCityAndType($city, $type)
+    {
+        $city_incidents = $this->incident->select(DB::raw('city, type, status, count(*) as number_of_incidents'))
+            ->where('city',$city)
+            ->where('status','approved')
+            ->where('type', $type)
             ->groupBy('type')
             ->get();
-        dd($district_incidents->toArray());
+        if(count($city_incidents) > 0)
+        {
+            return $city_incidents->toArray()[0]['number_of_incidents'];
+        }
     }
 }
---
+++ resources/views/public/district.blade.php
@@ -0,0 +1,96 @@
+@extends($template)
+
+@section('content')
+    <style>
+        html, body {
+            height: 100%;
+            margin-top: 0;
+        }
+
+    </style>
+    <!--Load the AJAX API-->
+    <script type="text/javascript" src="{{ asset('https://www.gstatic.com/charts/loader.js') }}"></script>
+    <script type="text/javascript">
+        @if(isset($cities))
+            google.charts.load('current', {'packages':['bar']});
+            google.charts.setOnLoadCallback(drawChart);
+
+            var chartData = [];
+            chartData[0] = ['City', 'Landslide', 'Thunderstorm', 'Flood', 'Fire', 'Other'];
+
+            function drawChart() {
+
+                @foreach($cities as $city => $incident)
+                    chartData.push(
+                        [
+                            '{{ $city }}',
+                            parseInt('{{ $incident['Landslide'] ? $incident['Landslide'] : 0}}'),
+                            parseInt('{{ $incident['Thunderstorm'] ? $incident['Thunderstorm'] : 0}}'),
+                            parseInt('{{ $incident['Flood'] ? $incident['Flood'] : 0}}'),
+                            parseInt('{{ $incident['Fire'] ? $incident['Fire'] : 0}}'),
+                            parseInt('{{ $incident['Other'] ? $incident['Other'] : 0}}')
+                        ]
+                    );
+                    {{--console.log( ' {{ $city }} ' );--}}
+                    {{--console.log( ' Landslide: {{ isset($city['Landslide'] ) ? $city['Landslide'] : 0 }}' );--}}
+                    {{--console.log( ' Thunderstorm: {{ isset($city['Thunderstorm'] ) ? $city['Landslide'] : 0 }}' );--}}
+                    {{--console.log( ' Flood: {{ isset($city['Flood'] ) ? $city['Flood'] : 0 }}' );--}}
+                    {{--console.log( ' Fire: {{ isset($city['Fire'] ) ? $city['Fire'] : 0 }}' );--}}
+                    {{--console.log( ' Other: {{ isset($city['Other'] ) ? $city['Other'] : 0 }}' );--}}
+                @endforeach
+
+                console.log(chartData);
+
+                var data = google.visualization.arrayToDataTable(chartData);
+
+                var options = {
+                    chart: {
+                        title: 'Incidents in {{ isset($district) ? $district : 'Not set' }}'
+//                        subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                    },
+                    colors: ['#795548', '#9e9e9e', '#2196f3', '#f44336', '#9c27b0']
+                };
+
+                var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                chart.draw(data, google.charts.Bar.convertOptions(options));
+            }
+        @endif
+    </script>
+
+    <div class="container" style="{{ isset($user_type) ? 'margin-top: 80px;' : 'margin-top: 20px;' }} height: 100%">
+
+        <div class="row" style=" height: 100%;">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-12" style="border: 1px solid red;  height: 100%;">
+                @if(isset($cities) && isset($district))
+                    <h1>{{ $district }}<small> District</small></h1>
+
+                    <div>
+                        <div style="width: 100%;">
+                            <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
+                        </div>
+                        <table>
+                            <thead>
+                            <th>GDGD</th>
+                            <th>GDGD</th>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>DGD</td>
+                                <td>DGD</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                @endif
+
+
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+    </div>
+@endsection
