dee87ad83bbe7e09b33aae515e978d062213de8c
sahirug <sahirug@gmail.com>
2017-12-17 04:23:01 +0530

    Changes

---
+++ resources/views/delete_pictures.blade.php
@@ -0,0 +1,22 @@
+@extends('templates.admin_template')
+
+@section('content')
+
+    <div class="container" style="margin-top: 50px; margin-bottom: 50px;">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-12">
+
+                <h1 class="my-4">Delete Pictures
+                </h1>
+
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+    </div>
+
+@endsection
